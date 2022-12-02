7ec7793c575eca1eeb4b8545fedd92e41a0083d5
Angellyn218 <angellyncervantes@yahoo.com>
2022-12-01 22:00:10 -0800

    Fixed bug in db put and get

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,9 +17,17 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put(content);
-  const result = await request;
-  console.log('data saved to database', result);
+  const requestGet = store.get(1);
+  const resultGet = await requestGet;
+  if (resultGet.data) {
+    const request = store.put({id: 1, data: resultGet + content});
+    const result = await request;
+    console.log('data added to database', result);
+  } else {
+    const request = store.put({id: 1, data: content});
+    const result = await request;
+    console.log('data saved to database', result);
+  }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -27,7 +35,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.get();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
   return result;
